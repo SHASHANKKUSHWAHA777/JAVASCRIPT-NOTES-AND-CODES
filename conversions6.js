a4250b298aefc39af923895bdc1cb98093c0228b
Shashank Kushwaha <kushwahashashank71@gmail.com>
2026-01-08 19:26:33 +0530

    Add files via upload
    these all things are all serial wise . ignore the numberings 4 , 5 , 6 . refer to it as a sequence . as it is my learnings , the codes i wrote after i learned these things from youtube , documentations , ai and different courses , so i labelled 4,5,6 etc as sequence , so no need to worry.

---
+++ conversions6.js
@@ -0,0 +1,31 @@
+let val = "33abc"
+
+let isnumber = Number(val)
+console.log(typeof isnumber);
+console.log(isnumber);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let val2 = "shashank"
+
+let isstr = String(val2)
+console.log(typeof isstr);
+console.log(isstr);
+
+let val3 = 1
+let checkbool = Boolean(val3)
+console.log(checkbool);
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let val4 = 33
+let convs = String(val4)
+console.log(typeof convs);
+
+console.log(convs);
+
+
+
---
+++ datatypes5.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high //so make it as console.log(3+3) i.e. code should be readable and in 1 line.
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof false);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
