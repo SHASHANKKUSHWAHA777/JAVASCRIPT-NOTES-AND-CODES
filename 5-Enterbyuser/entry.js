b447d5b653c14fe1c1bbf7b3a40bfa008111d689
Shashank Kushwaha <kushwahashashank71@gmail.com>
2026-01-17 15:35:51 +0530

    Create entry.js

---
+++ 5-Enterbyuser/entry.js
@@ -0,0 +1,13 @@
+//program to enter a name from user in javascript
+const readline=require("readline")
+const r1=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+r1.question("enter your name: ",(name) =>{
+    console.log("Hello",name)
+    r1.close()
+})
+
+
